96d5e7a1c950aaf6a1e6c09fc887f11bcf7f9a12
MarkoVitkovic <marko.vitkovic@outlook.com>
2020-03-28 01:47:00 +0100

    add cards to hotels and food

--- src/components/pages/ThingsToDo.js
+++ src/components/pages/ThingsToDo.js
@@ -4,8 +4,9 @@ export default class ThingsToDo extends Component {
     render() {
         return (
             <div>
-                todo
+            
             </div>
+            
         )
     }
 }
